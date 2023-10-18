120eb912aaedfd64a2345ca3beaa46d4675cf990
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-10-18 21:18:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -8,46 +7,30 @@ const images = [
   { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-function downloadImage(){
-	//clear the existing content in the output div
-	output.innerHTML = '';
+function downloadImages() {
+  // Clear the existing content in the output div
+  output.innerHTML = '';
 
-	//create an array of promises to download the images
-	const imagePromises = images.map((image)=>{
-		return new Promise((resolve, reject)=>{
-			const imageElement = new Image()
-			imageElement.onload = ()=> resolve(imageElement)
-			imageElement.onerror = ()=> reject(new Error(`Failed to load images's URL: $(image.url)`))
-			imageElement.src = image.url
-		})
-	})
+  // Create an array of promises to download the images
+  const imagePromises = images.map((image) => {
+    return new Promise((resolve, reject) => {
+      const imgElement = new Image();
+      imgElement.onload = () => resolve(imgElement);
+      imgElement.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+      imgElement.src = image.url;
+    });
+  });
 
-	//use promise.all to download images in parallel
-
-	Promise.all(imagePromises).then((images)=>{
-		images.forEach((img)=>{
-			output.appendChild(img)
-		})
-	}).catch((error)=>{
-		output.textContent = error.message
-	})
-	
+  // Use Promise.all to download images in parallel
+  Promise.all(imagePromises)
+    .then((images) => {
+      images.forEach((img) => {
+        output.appendChild(img);
+      });
+    })
+    .catch((error) => {
+      output.textContent = error.message;
+    });
 }
 
-btn.addEventListener("click", downloadImage)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn.addEventListener("click", downloadImages);
